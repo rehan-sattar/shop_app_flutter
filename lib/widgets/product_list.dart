bd20cd63382e6caa2b6f128707ba93212f520c2f
rehan-sattar <rehansattar117@gmail.com>
2020-04-19 23:42:39 +0500

    📦 NEW: add provider for products

--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -1,33 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import './product_item.dart';
-import '../providers/products.dart';
+import './products_grid.dart';
 
 class ProductList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Shop'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, i) => ProductItem(
-          products[i].id,
-          products[i].title,
-          products[i].imageUrl,
-        ),
-        itemCount: products.length,
-      ),
+      body: ProductsGrid(),
     );
   }
 }
